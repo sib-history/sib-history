54897a01599c7399595020dc3fa2017a965d581c
User <lukashevich.work@gmail.com>
2018-01-31 02:35:14 +0300

    all index pages

--- js/main.js
+++ js/main.js
@@ -1,27 +1,32 @@
-   $('.header__menu-mobile-icon').on('click', function () {
-       $('.header__menu-list').slideToggle();
-   });
-   $('.projects-page__item-preview').on('click', function () {
-       $(this).parent().addClass('_active');
-   });
+function checkElementExistance (checkSelector) {
+    return $(checkSelector).length >= 1;
+}
 
-   $('.news-slider').slick({
-       slidesToShow: 3,
-       slidesToScroll: 1,
-       autoplay: true,
-       autoplaySpeed: 4000,
-       responsive: [
-           {
-               breakpoint: 768,
-               settings: {
-                   slidesToShow: 2
-               }
-           },
-           {
-               breakpoint: 480,
-               settings: {
-                   slidesToShow: 1
-               }
-           }
-       ]
-   });
+$('.header__menu-mobile-icon').on('click', function () {
+    $('.header__menu-list').slideToggle();
+});
+$('.projects-page__item-preview').on('click', function () {
+    $(this).parent().addClass('_active');
+});
+if (checkElementExistance('.news-slider')) {
+    $('.news-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+}
