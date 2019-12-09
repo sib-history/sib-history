2ea28d9f94d26d7a4b818dbe417c6fbb76d9aea8
Denis Lukashevich <denis.lukashevich@megafon.ru>
2019-12-09 17:36:31 +0300

    add about error boundaries

--- js/about_scripts.js
+++ js/about_scripts.js
@@ -3,12 +3,25 @@ fillPage();
 async function fillPage() {
     switchLoading(true);
 
-    let data = await app.content.get('about', { populate: [ 'cover' ] });
+
+    let data = {};
+    await app.content.get('about', { populate: [ 'cover' ] }).then((result) => {
+        data = result;
+    }).catch(async (e) => {
+        console.warn(e);
+        if(e.code === 'storage/quota-exceeded') {
+            await app.content.get('about', { fields: [ 'title', 'article' ] }).then((result)=> {
+                data = result;
+            }).catch((e)=> {
+                console.error(e);
+            })
+        }
+    });
 
 
     $('.feature__heading').html(data.title);
     $('.article').html(data.article);
-    let sizedUrl = sizeFlamelinkImg(data.cover[0].url, '1920_9999_100');
+    let sizedUrl = (data.cover && data.cover[0] && sizeFlamelinkImg(data.cover[0].url, '1920_9999_100')) || 'img/about-header.jpg';
     $('.feature__inner').css('background-image', 'url("'+sizedUrl+'")');
 
     switchLoading(false);
